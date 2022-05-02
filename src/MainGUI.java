21bcad7db64bfeeac293a93a2737f3e62760dab8
c-a-g-e <cagecpeterson8@gmail.com>
2022-05-02 12:37:08 -0700

    Worked on MainGUI class, started testing

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,4 +1,7 @@
+import com.sun.tools.javac.Main;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,20 +10,51 @@ public class MainGUI extends JFrame implements ActionListener {
     private JFrame mainGui;
 
     private JPanel startMenu;
-    private JButton startButton;
-    private JButton settingsButton;
-    private JButton exitButton;
+    private JLabel titleText;
+    private ButtonGroup startMenuButtonGroup;
+    private JButton startButton = new JButton("Start");
+    private JButton settingsButton = new JButton("Settings");
+    private JButton exitButton = new JButton("Exit");
 
     private JPanel characterSelectMenu;
     private JPanel mapScreen;
     private JPanel combatScreen;
 
-
-
+    public static void main(String[] args) {
+        new MainGUI();
+    }
 
     public MainGUI() {
         mainGui = new JFrame();
+        this.setTitle("Dungeon Adventure");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 800);
+        setStartMenuPanel();
+        this.setVisible(true);
+    }
+
+    private void setStartMenuPanel() {
         startMenu = new JPanel();
+
+        titleText = new JLabel();
+        titleText.setText("Dungeon Adventure");
+        startMenu.add(titleText);
+
+        startMenuButtonGroup = new ButtonGroup();
+        startButton = new JButton();
+        settingsButton = new JButton();
+        exitButton = new JButton();
+
+        startMenuButtonGroup.add(startButton);
+        startMenuButtonGroup.add(settingsButton);
+        startMenuButtonGroup.add(exitButton);
+
+        startMenu.add(startButton, BoxLayout.Y_AXIS);
+        startMenu.add(settingsButton, BoxLayout.Y_AXIS);
+        startMenu.add(exitButton, BoxLayout.Y_AXIS);
+
+        this.add(startMenu);
+        startMenu.setVisible(true);
     }
 
     @Override
